dffc811a667e973a236978dba3a22b723283fe9b
ath <seof622@naver.com>
2024-11-07 19:48:43 -0800

    feat: add Module File for Arm Operation

---
+++ AGV_Team_1/ArmServo.py
@@ -0,0 +1,71 @@
+from SCSCtrl import TTLServo
+from jetbot import Robot
+import time
+
+class AGVServo:
+    speed_motor = 150
+    dir = -1
+    motor_degree = [-1, 0, 0, 0, 0, 0]
+    sensitivity_degree = 5
+    robot = Robot()
+
+    def __init__(self):
+        for i in range(1, 6):
+            TTLServo.servoAngleCtrl(i, self.motor_degree[i], self.dir, self.speed_motor)
+        self.robot.stop()
+
+    def check_in_range(self, degree):
+        if degree > 150:
+            return 150
+        if degree < -150:
+            return -150
+        return degree
+
+    def update_degree(self, idx, degree):
+        self.motor_degree[idx] = self.check_in_range(degree)
+
+    """
+    idx meaning
+    1 : horizontal arm
+    2 : vertical arm 1
+    3 : vertical arm 2
+    4 : Grip
+    5 : Camera
+    """
+    def operate_arm(self, idx, degree):
+        if idx < 1 or idx > 5:
+            pass
+        self.update_degree(idx, degree)
+        TTLServo.servoAngleCtrl(
+            idx,
+            self.motor_degree[idx],
+            self.dir,
+            self.speed_motor
+        )
+
+    def stop(self):
+        self.robot.stop()
+
+    def move(self, direction, speed, duration):
+        if direction == "f" or direction == "F":
+            self.robot.forward(speed)
+        elif direction == "b" or direction == "B":
+            self.robot.backward(speed)
+        elif direction == "r" or direction == "R":
+            self.robot.right(speed)
+        elif direction == "l" or direction == "L":
+            self.robot.left(speed)
+
+        time.sleep(duration)
+        self.stop()
+
+class AGVTeamOneServo(AGVServo):
+    def __init__(self):
+        self.motor_degree = [-1, 8, 0, 0, 0, 0]
+        super().__init__()
+
+class AGVTeamTwoServo(AGVServo):
+    def __init__(self):
+        self.motor_degree = [-1, 6, 11, 7, 0, 4]
+        super().__init__()
+
